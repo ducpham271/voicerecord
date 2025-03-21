05862553551803264bf4e9a2e8323bd1739846c9
D P <dp@Ds-MacBook-Air.local>
2025-03-21 23:36:01 +0700

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from audiorecorder import audiorecorder
+import datetime
 
 st.title("Audio Recorder")
 audio = audiorecorder("Click to record", "Click to stop recording", custom_style={"backgroundColor": "lightblue"})
@@ -9,7 +10,12 @@ if len(audio) > 0:
     st.audio(audio.export().read())
 
     # To save audio to a file, use pydub export method:
-    audio.export("audio.wav", format="wav")
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y%m%d_%H%M%S")  # Format: YYYYMMDD_HHMMSS
+    filename = f"audio_{timestamp}.wav"
+
+    audio.export(filename, format="wav")
+    print(filename)
 
     # To get audio properties, use pydub AudioSegment properties:
     st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
